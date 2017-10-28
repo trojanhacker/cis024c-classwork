ca40a4e23889ca60fc4b8e6aea0ea9b9b5b1b670
Sanjay Dorairaj <Sanjay_Dorairaj@cable.comcast.com>
2017-10-28 09:37:15 -0700

    week8 classwork

---
+++ week8/mysimplemath.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[1]:
+
+def add(a,b):
+    return a+b
+
+def multiply(a,b):
+    return a*b
+
+def getmax(a,b):
+    return max([a,b])
+
+
+# In[ ]:
+
+
+
---
+++ week8/simple_library.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[2]:
+
+def addAll(inList):
+    
+    return sum(inList)
+
+def multiplyAll(inList):
+    
+    return reduce(lambda x,y: x*y,inList)
+
